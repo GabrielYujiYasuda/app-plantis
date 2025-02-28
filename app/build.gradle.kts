b64286c2da02f9cd765fececf6748c1ebf3900c4
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2025-02-27 21:35:26 -0800

    feat: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
+import org.gradle.kotlin.dsl.libs
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
     namespace = "com.example.plantis"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.plantis"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -40,6 +43,23 @@ android {
 }
 
 dependencies {
+    //region Serialization
+    implementation(libs.kotlin.serialization)
+    //endregion
+
+    //region Navigation Compose
+    implementation(libs.navigation.compose)
+    //endregion
+
+    //region Dependency Injection - Coil
+    implementation(libs.coil.compose)
+    //endregion
+
+    //region Ktor
+    implementation(platform(libs.ktor.bom))
+    implementation(libs.bundles.ktor)
+    //endregion
+
     //region JUnit
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -54,7 +74,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.ui.tooling)
