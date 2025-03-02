a78b65725d5a496758262e928dedffb85abe7dad
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2025-03-02 10:52:46 -0800

    feat: add typography and style

--- app/src/main/java/com/example/plantis/ui/theme/Color.kt
+++ app/src/main/java/com/example/plantis/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.plantis.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val GreenLight = Color(0xFF8BC34A)
+val GreenBase = Color(0xFF4CAF50)
+val GreenDark = Color(0xFF2E7D32)
+val GreenExtraLight = Color(0xFFE8F5E9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BrownLight = Color(0xFFBCAAA4)
+val BrownBase = Color(0xFF8D6E63)
+val BrownDark = Color(0xFF5D4037)
+
+val YellowLight = Color(0xFFFFF9C4)
+val YellowBase = Color(0xFFFFEB3B)
+val YellowDark = Color(0xFFFBC02D)
+
+val Gray100 = Color(0xFFFAFAFA)
+val Gray200 = Color(0xFFF5F5F5)
+val Gray300 = Color(0xFFEEEEEE)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray500 = Color(0xFF757575)
+val Gray600 = Color(0xFF424242)
--- app/src/main/java/com/example/plantis/ui/theme/Type.kt
+++ app/src/main/java/com/example/plantis/ui/theme/Type.kt
@@ -2,33 +2,65 @@ package com.example.plantis.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.plantis.R
 
-// Set of Material typography styles to start with
+private const val activatePreview = true
+
+val nunitoFontFamily = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_bold, FontWeight.Bold),
+    Font(R.font.nunito_semi_bold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineMedium = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = if (activatePreview) FontFamily.Default else nunitoFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
     )
-    */
 )
